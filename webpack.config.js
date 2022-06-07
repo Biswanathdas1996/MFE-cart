8ff8f0ecf4bb92f4a17cc816e20ff5da09adee01
your_username <your_email_address@example.com>
2022-06-07 19:31:06 +0530

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require("path");
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
+    // publicPath: "http://localhost:8101/",
     publicPath: "https://mfe-cart.netlify.app/",
   },
 
